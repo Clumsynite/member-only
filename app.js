8d55a923aff2b9ed9848c8efb8b7728e6b72b703
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-02 19:44:09 +0530

    initialised app.js

---
+++ app.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const mongoDB = process.env.MONGO_URL || process.env.MONGO_URI;
+
+mongoose.connect(mongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
+
+const indexRouter = require("./routes/index");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(express.json());
+app.use("/", indexRouter);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(express.urlencoded({extended: false}))
+
+app.listen(3000, () => {console.log(`App listening on port 3000!\nhttp://localhost:3000`)})
