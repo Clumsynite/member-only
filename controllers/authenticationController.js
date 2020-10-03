96fbfe9c73d94e96c5986086f9f697234823bb42
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-03 19:36:30 +0530

    added signup get and post functions

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,8 +1,27 @@
 const User = require("../models/user");
-const async = require("async");
+const bcryptjs = require("bcryptjs");
 
 exports.signup_get = (req, res, next) => {
-  res.render("signup_form", { title: "Sign Up" });
+  res.render("signup_form", { title: "SignUp" });
+};
+
+exports.signup_post = (req, res, next) => {
+  bcryptjs.hash(req.body.password, 10, (err, hashedPassword) => {
+    if (err) {
+      return next(err);
+    }
+    const user = new User({
+      username: req.body.username,
+      password: hashedPassword,
+      fname: req.body.fname,
+      lname: req.body.lname,
+    }).save((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  });
 };
 
 exports.login_get = (req, res, next) => {
