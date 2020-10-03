b1dbbfa0163f663fe922dcf3ad66638f9271b8d8
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-03 16:22:23 +0530

    changed routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router()
 
 const authenticationController = require('../controllers/authenticationController');
+const messageController = require('../controllers/messageController');
 
-router.get('/', authenticationController.signup_get)
+router.get('/', messageController.get_messages)
+
+router.get('/login', authenticationController.login_get)
+
+router.get('/signup', authenticationController.signup_get)
 
 module.exports = router
