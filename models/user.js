2c523ce98aa0bdc9a9430961749572aca646078c
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-03 16:19:19 +0530

    updated user model
    added first name and last name objects
    added a virtual for fullname

--- models/user.js
+++ models/user.js
@@ -2,8 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+  fname: { type: String, required: true },
+  lname: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
 });
+
+UserSchema.virtual('fullname').get(function (){
+  return `${this.fname} ${this.lname}`
+})
 
 module.exports = mongoose.model('User', UserSchema)
