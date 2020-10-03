98113508ee3af26f15e6cf5050eb4cf91ead9a66
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-03 16:18:27 +0530

    Added message model

---
+++ models/message.js
@@ -0,0 +1,16 @@
+const moment = require('moment');
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  title: { type: String, require: true },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  content: { type: String, require: true },
+  added: { type: Datetring, default: Date.now },
+});
+
+MessageSchema.virtual("timestamp", function () {
+  return moment(this.added).format("MMMM Do YYYY, h:mm:ss a");
+});
+
+module.exports = mongoose.model("Message", MessageSchema);
