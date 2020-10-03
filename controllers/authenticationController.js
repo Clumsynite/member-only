bb1d3d1e3a71cc63e9090d08feb41b0c0be5b60d
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-03 16:17:38 +0530

    changed rendered forms

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,6 +1,14 @@
-const User = require('../models/user')
-const async = require('async');
+const User = require("../models/user");
+const async = require("async");
+
+exports.signup_get = (req, res, next) => {
+  res.render("signup_form", { title: "Sign Up" });
+};
 
 exports.login_get = (req, res, next) => {
-  res.render('login_form')  
-}
+  res.render("login_form", { title: "Login" });
+};
+
+exports.login_post = (req, res, next) => {
+  res.render("login_form", { title: "Login" });
+};
