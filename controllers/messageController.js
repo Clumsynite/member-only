1921e3c10acef334f2368062287be517b4d49ba0
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-04 17:35:18 +0530

    Added controller to get messages and add message

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,9 +1,28 @@
-const Message = require('../models/message');
-const async = require('async');
+const Message = require("../models/message");
+const async = require("async");
 
 exports.get_messages = (req, res, next) => {
-  res.render('index', {user: req.user, state: req.isAuthenticated()})
-}
+  Message.find({}).populate('author').exec((err, data) => {
+    if (err) {
+      return next(err);
+    }
+    res.render("index", { user: req.user, state: req.isAuthenticated(), messages: data });
+  });
+};
 exports.new_message_get = (req, res, next) => {
-  res.render('message_form', {title: "New Message"})
-}
+  res.render("message_form", { title: "New Message" });
+};
+exports.new_message_post = (req, res, next) => {
+  const message = new Message({
+    title: req.body.title,
+    content: req.body.content,
+    author: req.user._id,
+  });
+  message.save((err, data) => {
+    if (err) {
+      res.render("message_form", { errors: err });
+      return;
+    }
+    res.redirect("/");
+  });
+};
