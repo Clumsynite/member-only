36c67eb1910b1b15b694fc9005d1133aaa01c6b8
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-04 15:45:53 +0530

    refactored app.js ro execute middlewares in order

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
-const flash = require('connect-flash');
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
@@ -12,7 +11,8 @@ const User = require("./models/user");
 const mongoose = require("mongoose");
 
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const mongoDB = process.env.MONGO_URL || process.env.MONGO_URI;
 
 mongoose.connect(mongoDB, {
@@ -25,42 +25,35 @@ const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "MongoDB connection error: "));
 
-const MongoStore = require('connect-mongo')(session);
-const connection = mongoose.createConnection(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-const sessionStore = new MongoStore({ mongooseConnection: connection, collection: 'sessions' });
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-app.use(flash())
-app.use(
-  session({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-    store: sessionStore,
-    cookie: {maxAge: 10 * 3000}
-  })
-);
+const MongoStore = require("connect-mongo")(session);
+const connection = mongoose.createConnection(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const sessionStore = new MongoStore({
+  mongooseConnection: connection,
+  collection: "sessions",
+});
 
 passport.use(
   new LocalStrategy((username, password, done) => {
-    User.findOne({ username }, (err, user) => {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false, { msg: "Incorrect username" });
-      }
-      bcryptjs.compare(password, user.password, (err, res) => {
-        if (res) {
-          console.log(res)
-          return done(null, user);
-        } else {
-          return done(null, false, { msg: "Incorrect password" });
+    User.findOne({ username: username })
+      .then((user) => {
+        if (!user) {
+          return done(null, false, { msg: "Incorrect username" });
         }
+        bcryptjs.compare(password, user.password, (err, res) => {
+          if (res) {
+            console.log(res);
+            return done(null, user);
+          } else {
+            return done(null, false, { msg: "Incorrect password" });
+          }
+        });
+      })
+      .catch((err) => {
+        done(err);
       });
-      return done(null, user);
-    });
   })
 );
 
@@ -69,14 +62,27 @@ passport.serializeUser((user, done) => {
 });
 passport.deserializeUser((id, done) => {
   User.findById(id, (err, user) => {
-    done(err, user);
+    if(err) {return done(err)}
+    done(null, user);
   });
 });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: { maxAge: 10 * 3000 },
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
