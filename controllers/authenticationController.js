8e9890583424b88d294cbee0c734eab2cddb8e0a
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-04 15:46:49 +0530

    improved error handling
    Render errors on login and signup
    Made username check

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -6,28 +6,50 @@ exports.signup_get = (req, res, next) => {
   res.render("signup_form", { title: "SignUp" });
 };
 
-exports.signup_post = (req, res, next) => {
-  bcryptjs.hash(req.body.password, 10, (err, hashedPassword) => {
-    if (err) {
-      return next(err);
-    }
-    const user = new User({
-      username: req.body.username,
-      password: hashedPassword,
-      fname: req.body.fname,
-      lname: req.body.lname,
-    }).save((err) => {
+exports.signup_post = async (req, res, next) => {
+  const exists = await  User.findOne({username: req.body.username})
+  if(exists) {
+    res.render('signup_form', {title: 'SignUp', errors: {msg: 'Username already exists'}, fname: req.body.fname, lname: req.body.lname})
+  }else{
+    bcryptjs.hash(req.body.password, 10, (err, hashedPassword) => {
       if (err) {
         return next(err);
       }
-      res.redirect("/");
+      const user = new User({
+        username: req.body.username,
+        password: hashedPassword,
+        fname: req.body.fname,
+        lname: req.body.lname,
+      }).save((err) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect("/");
+      });
     });
-  });
+  
+  }
 };
 
 exports.login_get = (req, res, next) => {
   res.render("login_form", { title: "Login" });
 };
+
+exports.login_post = (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+
+    if (!user) {
+      res.render("login_form", { title: "Login", errors: info, username: req.body.username });
+      return
+    }
+    req.logIn(user, function (err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  })(req, res, next);
+}
 
 exports.logout_get = (req, res, next) => {
   req.session.destroy((err) => {
