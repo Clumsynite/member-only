35bf65b208059edf7c58a2f6a38346429b54662c
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-04 17:04:46 +0530

    adding new_message_get to render message form

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -4,3 +4,6 @@ const async = require('async');
 exports.get_messages = (req, res, next) => {
   res.render('index', {user: req.user, state: req.isAuthenticated()})
 }
+exports.new_message_get = (req, res, next) => {
+  res.render('message_form', {title: "New Message"})
+}
