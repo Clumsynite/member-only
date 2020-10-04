364f164d82111e025d39dd2a7f8f81750519a1f7
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-04 15:53:48 +0530

    refactored and improved routing

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const passport = require('passport');
+const passport = require("passport");
 const express = require("express");
 const router = express.Router();
 
@@ -6,27 +6,29 @@ const authenticationController = require("../controllers/authenticationControlle
 const messageController = require("../controllers/messageController");
 
 const checkAuthenticated = (req, res, next) => {
-  if(req.isAuthenticated()) {
-    return next()
-  } 
-  res.redirect('/login')
-}
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+};
 
 router.get("/", messageController.get_messages);
 
 router.get("/login", authenticationController.login_get);
 
-router.post(
-  "/login", passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-  })
-);
+// router.post(
+//   "/login", passport.authenticate('local', {
+//     successRedirect: '/',
+//     failureRedirect: '/login',
+//     failureFlash: true
+//   })
+// );
+
+router.post("/login", authenticationController.login_post);
 
 router.get("/signup", authenticationController.signup_get);
 
 router.post("/signup", authenticationController.signup_post);
 
-router.get('/logout', authenticationController.logout_get)
+router.get("/logout", authenticationController.logout_get);
 module.exports = router;
